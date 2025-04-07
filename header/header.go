da61e3120eb4e03ff6646eaec0485922b555e892
vault-thirteen <54213347+vault-thirteen@users.noreply.github.com>
2025-04-07 17:50:00 +0300

    Update of 'header' : mail

--- header/header.go
+++ header/header.go
@@ -2,15 +2,6 @@ package header
 
 // Common message header names for various protocols.
 
-// This package contains all IANA registered header names which have an RFC
-// reference as of 2023-03-21.
-// For more information visit this URL:
-// https://www.iana.org/assignments/message-headers/message-headers.xml
-// This list may contain some old deprecated and obsolete header names.
-//
-// HTTP field name registrations have been moved to:
-// https://www.iana.org/assignments/http-fields/http-fields.xhtml
-
 // Delimiters.
 const (
 	DelimiterCommaSpace = ", "
