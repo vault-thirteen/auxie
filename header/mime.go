31f70ae4ab027a73ff4ce09e69fa3d400dfd5577
Vault Thirteen <54213347+vault-thirteen@users.noreply.github.com>
2026-04-18 15:58:56 +0300

    'header' package: update of MIME headers.

--- header/mime.go
+++ header/mime.go
@@ -2,17 +2,6 @@ package header
 
 // MIME protocol header field names.
 
-// Status: Obsoleted.
-const (
-	MimeHeaderBase        = "Base"         // [RFC1808][RFC2068 Section 14.11]
-	MimeHeaderContentBase = "Content-Base" // [RFC2110][RFC2557]
-)
-
-// Status: Standard.
-const (
-	MimeHeaderContentTranslationType = "Content-Translation-Type" // [RFC8255]
-)
-
 // Status: (Empty).
 const (
 	MimeHeaderContentAlternative      = "Content-Alternative"       // [RFC4021]
@@ -25,6 +14,17 @@ const (
 	MimeHeaderContentLocation         = "Content-Location"          // [RFC4021]
 	MimeHeaderContentMD5              = "Content-MD5"               // [RFC4021]
 	MimeHeaderContentTransferEncoding = "Content-Transfer-Encoding" // [RFC4021]
-	MimeHeaderContentType             = "Content-Type"              // [RFC4021][RFC-ietf-lamps-header-protection-25]
+	MimeHeaderContentType             = "Content-Type"              // [RFC4021][RFC9788]
 	MimeHeaderMIMEVersion             = "MIME-Version"              // [RFC4021]
+)
+
+// Status: Obsoleted.
+const (
+	MimeHeaderBase        = "Base"         // [RFC1808][RFC2068 Section 14.11]
+	MimeHeaderContentBase = "Content-Base" // [RFC2110][RFC2557]
+)
+
+// Status: Standard.
+const (
+	MimeHeaderContentTranslationType = "Content-Translation-Type" // [RFC8255]
 )
