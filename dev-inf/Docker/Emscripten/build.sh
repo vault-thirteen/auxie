bed86be83f1395dc551587735ea23f63b1cd1233
Vault Thirteen <54213347+vault-thirteen@users.noreply.github.com>
2026-09-24 22:21:28 +0300

    Update of the 'dev-inf' folder

--- dev-inf/Docker/Emscripten/build.sh
+++ dev-inf/Docker/Emscripten/build.sh
@@ -6,7 +6,7 @@ set -e
 img_hash_file="image_hash.txt"
 
 docker build --file Emscripten.dockerfile .
-docker images --no-trunc --format "{{.ID}}" | head -n 1 > $img_hash_file
+docker images --no-trunc -a -q | head -n 1 > $img_hash_file
 
 img_hash=$(cat $img_hash_file)
 echo "Image hash sum: $img_hash"
